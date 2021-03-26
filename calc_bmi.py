1bd99c9846f8d8aa26fc0a09e0db87799b6211e5
arayashiki kyo <k.arayashiki.sys20@morijyobi.ac.jp>
2021-03-26 11:01:38 +0900

    BMI計算アプリの新規作成

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def bmi(sin,tai):
+    sin2 = sin /100
+    b = tai / (sin2*sin2)
+    return b
---
+++ main.py
@@ -0,0 +1,5 @@
+import calc_bmi
+print("BMI計算アプリ")
+a = int(input("身長："))
+b = int(input("体重："))
+print(f"BMI:{calc_bmi.bmi(a,b)}")
